1e305681dab2d6c14c4a3ecc998806c67fbcf84e
Amit Prajapati <aprajapati@routesme.com>
2020-04-27 18:14:59 +0530

    intial project commit

---
+++ TrackService/TrackServiceHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackService
+{
+    public class TrackServiceHub : Hub
+    {
+        //public async Task SendCordinates(string VehicleId, string latitude, string longitude, string datetime)
+        //{
+        //    await Clients.All.SendAsync("GetCordinates", VehicleId, latitude, longitude, datetime);
+        //}
+    }
+}
