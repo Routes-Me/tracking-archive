847386cf73aae9004f32278c974f97afc250aef8
Mariam AbdelRazek <mRazek@routesme.com>
2020-05-05 16:54:05 +0200

    .

--- TrackService/Models/TrackingInformation.cs
+++ TrackService/Models/TrackingInformation.cs
@@ -1,15 +1,11 @@
-using System;
 
 namespace TrackService.Models
 {
     public class TrackingInformation
     {
         public string VehicleId { get; set; }
-
         public string Longitude { get; set; }
-
         public string Latitude { get; set; }
-
         public string DateTime { get; set; }
     }
 }
